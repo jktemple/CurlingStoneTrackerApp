fd247faaf04fbf7fec1d4a22cee6327782e5681e
jktemple <jktemple@ucsc.edu>
2025-04-23 11:50:17 -0700

    Add project files.

---
+++ CurlingStoneTrackerApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CurlingStoneTrackerApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
