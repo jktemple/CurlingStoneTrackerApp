5e7fbe344332df1b0f92dd8c000a1a917459f945
jktemple <jktemple@ucsc.edu>
2025-04-23 15:08:16 -0700

    Adding plugin and android permissions

--- CurlingStoneTrackerApp/Platforms/Android/MainActivity.cs
+++ CurlingStoneTrackerApp/Platforms/Android/MainActivity.cs
@@ -7,5 +7,20 @@ namespace CurlingStoneTrackerApp
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            await Permissions.RequestAsync<BLEPermission>();
+        }
+    }
+
+    public class BLEPermission: Permissions.BasePlatformPermission
+    {
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
+            {
+                (Android.Manifest.Permission.BluetoothScan, true),
+                (Android.Manifest.Permission.BluetoothConnect, true)
+            }.ToArray();
     }
 }
