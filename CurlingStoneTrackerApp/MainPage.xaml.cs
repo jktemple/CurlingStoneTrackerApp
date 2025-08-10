bafb8306d75db82192e4886da526dca84539f76f
jktemple <jktemple@ucsc.edu>
2025-08-10 11:25:16 -0700

    Finally getting started on this project

--- CurlingStoneTrackerApp/MainPage.xaml.cs
+++ CurlingStoneTrackerApp/MainPage.xaml.cs
@@ -1,25 +1,68 @@
-﻿namespace CurlingStoneTrackerApp
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace CurlingStoneTrackerApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        private readonly IAdapter _bluetoothAdapter;
+        private readonly List<IDevice> _gattDevices = new List<IDevice>();
 
         public MainPage()
         {
             InitializeComponent();
+
+            _bluetoothAdapter = CrossBluetoothLE.Current.Adapter;
+            _bluetoothAdapter.DeviceDiscovered += (sender, foundBleDevice) =>
+            {
+                if (foundBleDevice != null && !string.IsNullOrEmpty(foundBleDevice.Device.Name))
+                    _gattDevices.Add(foundBleDevice.Device);
+            };
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async Task<bool> PermissionsGrantedAsync()      // Function to make sure that all the appropriate approvals are in place
         {
-            count++;
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            return status == PermissionStatus.Granted;
         }
+
+        private async void OnScanClicked(object sender, EventArgs e)
+        {
+            IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(ScanBtn.IsEnabled = false);
+            foundBleDevicesListView.ItemsSource = null;
+
+            if (!await PermissionsGrantedAsync()) {
+                await DisplayAlert("Permission required", "Application needs location permission", "OK");
+                IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(ScanBtn.IsEnabled = true);
+                return;
+            }
+
+            _gattDevices.Clear();
+
+            if (!_bluetoothAdapter.IsScanning) { 
+                await _bluetoothAdapter.StartScanningForDevicesAsync();
+            }
+
+            foreach (var device in _bluetoothAdapter.ConnectedDevices) { 
+                _gattDevices.Add(device);
+            }
+
+            foundBleDevicesListView.ItemsSource = _gattDevices.ToArray();
+            IsBusyIndicator.IsVisible = IsBusyIndicator.IsRunning = !(ScanBtn.IsEnabled = true);
+        }
+
+        private void OnFoundBluetoothDevicesListViewItemClicked(object sender, EventArgs e) 
+        { 
+
+        }
+
     }
 
 }
